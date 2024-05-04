28baad0066ae73078afd64bdb14243316236eea7
Johana <johanarabaez617@gmail.com>
2024-05-04 01:06:03 -0600

    creando server

--- server/server.ts
+++ server/server.ts
@@ -1,52 +1,49 @@
 import express from 'express';
-const app = express();
 
+const app = express();
 
 app.use(express.json());
 
 
-
-
 const data: Array<{}> = [
-   {
-       id: "1",
-       profilePicture:
-           "https://scontent.fmga3-1.fna.fbcdn.net/v/t39.30808-1/421878086_3662415144076963_6469529258262461884_n.jpg?stp=c0.0.480.480a_dst-jpg_p480x480&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=qL5OvfVhHukAb54Unf_&_nc_ht=scontent.fmga3-1.fna&oh=00_AfArKZm8hyKXYTPP3Nr0Y_SMXfHXE_czgoPSp3_a5x3hjg&oe=662C58DE",
-       userName: "Johana",
-       country: "Nicaragua",
-       city: "Managua",
-   },
-];
+    {
+        id: "1",
+        profilePicture:
+            "https://scontent-mia3-1.xx.fbcdn.net/v/t39.30808-1/302899535_1243389203080396_8459184593115578454_n.jpg?stp=dst-jpg_s200x200&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Lv7BBW2HCeAAb7A_P0-&_nc_ht=scontent-mia3-1.xx&oh=00_AfC9qs4YAVWYMmAodmVRPMR6eBSsugeFMg64qtqeZIURDw&oe=661F9E99",
+        image: "computer.png",
+        profileName: "Eliseo",
+    },
 
+];
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 
 
-
-
-
 app.get('/', async (req, res) => {
-   await wait(3000);
-   res.json(data);
+    await wait(3000);
+    res.json(data);
 });
-
-
 
 
 app.post('/posts', (req, res) => {
-   const data = req.body;
+    const { profilePicture = "",  profileName = "" } = req.body;
 
+    data.push({
+        id: data.length.toString(),
+        profilePicture,
+        profileName,
+    });
 
-   data.push(data);
+    console.log("Nuevo post recibido:");
+    console.log("Profile Picture:", profilePicture);
+   //  console.log("Image:", image);
+    console.log("Profile Name:", profileName);
 
-
-   res.json(data);
+    res.json(data);
 });
 
 
-
-
 app.listen(3000, () => {
-   console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
