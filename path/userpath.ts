f758bdedc83d21a07d9b896c6d7807fe4abb9bf0
Johana <johanarabaez617@gmail.com>
2024-05-04 01:12:37 -0600

    añadiendo path

---
+++ path/userpath.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+
+interface Post {
+  name: string;
+  location: string;
+  description: string;
+  imageUrl: string;
+  links: { url: string; text: string }[];
+  newlinks:  { url: string; text: string }[];
+}
+
+export const userpath = () => {
+
+    const [data, setData] = useState<Array<Post>>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const fetchPosts = async () => {
+        const response = await fetch("http://192.168.23.60:3000/");
+        const data: Array<Post> = await response.json();
+        console.log(data)
+
+        setData(data);
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    return { data, isLoading };
+}
